5c2a8beb6bcab826cad616bf51203ab58ce984cc
longjie <761166384@qq.com>
2019-06-26 09:23:04 +0800

    6-26-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CrisisListComponent} from './crisis-list/crisis-list.component';
 
-const routes: Routes = [];
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    {
+        path: 'crisis-center',
+        component: CrisisListComponent
+    },
+    {
+        path: '',
+        redirectTo: '/heroes',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CrisisListComponent} from './crisis-list/crisis-list.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HeroesModule} from './heroes/heroes.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CrisisListComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        HeroesModule,
+        FormsModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
